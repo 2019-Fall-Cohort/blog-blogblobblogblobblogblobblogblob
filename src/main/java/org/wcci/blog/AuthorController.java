83b50661dad6ea6c52f047140905f950bb8532a4
Diana McIntosh <dotteddice@yahoo.com>
2019-10-13 18:31:06 -0400

    template runs and shows author name

--- src/main/java/org/wcci/blog/AuthorController.java
+++ src/main/java/org/wcci/blog/AuthorController.java
@@ -1,5 +1,6 @@
 package org.wcci.blog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthorController {
 	
+	@Autowired
 	private AuthorStorage authorStorage;
 	
 	@GetMapping("/authors")
 	public String prepareAuthorView(Model model) {
-//		authorStorage.addAuthorToAvoidNullPointer();
+		authorStorage.addAuthorToAvoidNullPointer();
 		Iterable<Author> retrievedAuthors = authorStorage.findAllAuthors();
 		model.addAttribute("authors", retrievedAuthors);
 		return "authors";
